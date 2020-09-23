d577c09252d01fa01fc7e102b049f38cfc6cb21a
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2020-09-23 14:10:07 -0400

    Update to Terraform 0.13.3

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ export default (data) => {
         name: workspaceName,
         "auto-apply": true,
         "queue-all-runs": true,
-        "terraform-version": "0.13.0",
+        "terraform-version": "0.13.3",
       },
     },
   });
